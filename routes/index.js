ba161d1ffe6098ac4ada24092106736eaf444bae
hashandom <sampathdh199@gmail.com>
2024-08-04 02:53:38 +0530

    2024/aug/04

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const router = express.Router();
 
-const userDetailsController = require('../controller/userDetails')
-const userSignUpController = require('../controller/userSignUp');
-const userSignInController = require('../controller/userSignin');
+//user 
+const userDetailsController = require('../controller/user/userDetails')
+const userSignUpController = require('../controller/user/userSignUp');
+const userSignInController = require('../controller/user/userSignin');
 const authToken = require('../middleware/authToken');
-const userLogout = require('../controller/userLogout')
-const allUsers = require('../controller/allUsers')
-const updateUser = require('../controller/updateUser');
+const userLogout = require('../controller/user/userLogout')
+const allUsers = require('../controller/user/allUsers')
+const updateUser = require('../controller/user/updateUser');
+
+//product 
+const UploadProductController = require('../controller/product/uploadProduct')
+const getProductController = require('../controller/product/getProduct')
+const updateProductController  = require('../controller/product/updateProduct')
+const getCategoryProduct = require('../controller/product/getCategoryProduct')
 
 router.post("/signUp",userSignUpController);
 router.post("/login",userSignInController);
@@ -18,5 +25,11 @@ router.get("/userLogout",userLogout);
 //admin panel
 router.get("/all-user",authToken,allUsers)
 router.post("/update-user",authToken,updateUser)
+
+//products
+router.post("/upload-product",authToken,UploadProductController)
+router.get("/get-product",getProductController)
+router.post("/update-product",authToken,updateProductController)
+router.get("/get-category-product",getCategoryProduct)
 
 module.exports = router;
